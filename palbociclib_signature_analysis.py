15433defe673d27e596cecfb1611fa6900f9d6b3
Gang Yan <yangangmg@gmail.com>
2024-12-03 12:26:23 -0500

    update object and its methods

--- palbociclib_signature_analysis.py
+++ palbociclib_signature_analysis.py
@@ -9,67 +9,54 @@ from statsmodels.stats.multitest import fdrcorrection
 import warnings
 warnings.filterwarnings('ignore')
 
-def read_and_process_data():
-    # CARE data input
-    ccle_cdk6 = pd.read_csv("./CARE/CCLE_palbo_CDK6.csv")
-    ccle_rb1 = pd.read_csv("./CARE/CCLE_palbo_RB1.csv")
+class palbo_signatures:
+    def __init__(self, cdk6_fpath, rb1_fpath):
+    # check funtion for correct corresponding data
+        self.CDK6 = pd.read_csv(cdk6_fpath)
+        self.RB1 = pd.read_csv(rb1_fpath)
+        self.mutation_status()
     
-    # RNA-Seq data input
-    pal_seq = pd.read_csv("./RNA-seq/palbociclib_gene_list.txt", sep='\t')
-    cdk4ko_seq = pd.read_csv("./RNA-seq/cdk4ko_seq_gene_list.txt", sep='\t')
-    cdk6ko_seq = pd.read_csv("./RNA-seq/cdk6ko_seq_gene_list.txt", sep='\t')
-    
-    # CRISPR screen data
-    crispr_gene_summary = pd.read_csv('./control_normalized_working/palbo_vs_dmso_control_normalized.gene_summary.txt')
-    
-    # Filter sequencing data by fold change
-    pal_seq_select = pal_seq[abs(pal_seq['log2FoldChange']) >= 0.58][['GeneName', 'log2FoldChange', 'padj']]
-    cdk4ko_seq_select = cdk4ko_seq[abs(cdk4ko_seq['log2FoldChange']) >= 0.58][['GeneName', 'log2FoldChange', 'padj']]
-    cdk6ko_seq_select = cdk6ko_seq[abs(cdk6ko_seq['log2FoldChange']) >= 0.58][['GeneName', 'log2FoldChange', 'padj']]
-    
-    return ccle_cdk6, ccle_rb1, pal_seq_select, cdk4ko_seq_select, cdk6ko_seq_select, crispr_gene_summary
+    def mutation_status(self, pattern=r"^.*Mutation$"):
+        """add mutation status"""
+        self.CDK6['mutation'] = self.CDK6['Partner'].str.match(pattern)
+        self.RB1['mutation'] = self.RB1['Partner'].str.match(pattern)
 
-def mutation_status(data, pattern=r"^.*Mutation$"):
-    """Add mutation status column"""
-    data['mutation'] = data['Partner'].str.match(pattern)
-    return data
+    def _plot_scatter(self, data, title):
+        """Create volcano plot"""
+        plt.figure(figsize=(10, 8))
+        plt.scatter(data['t_value'], -np.log10(data['p_value']), 
+                c=data['mutation'].map({True: 'red', False: 'blue'}),
+                alpha=0.6)
+        plt.xlabel('t-value')
+        plt.ylabel('-log10(p-value)')
+        plt.title(title)
+        plt.show()
+    
+    def plot_volcano(self, target):
+        if target == "CDK6":
+            self._plot_scatter(self.CDK6, "Palbociclib signatures when CDK6 as primary target")
+        elif target == "RB1":
+            self._plot_scatter(self.RB1, "Palbociclib signatures when RB1 as primary target")
+        else:
+            raise ValueError("The target must be either CDK6 or RB1")
 
-def plot_volcano(data, title):
-    """Create volcano plot"""
-    plt.figure(figsize=(10, 8))
-    plt.scatter(data['t_value'], -np.log10(data['p_value']), 
-               c=data['mutation'].map({True: 'red', False: 'blue'}),
-               alpha=0.6)
-    plt.xlabel('t-value')
-    plt.ylabel('-log10(p-value)')
-    plt.title(title)
-    plt.show()
-
-def cross_heatmap(df1, df2, col2draw=None):
-    """Generate correlation heatmap between two datasets"""
-    if col2draw is None:
-        col2draw = ['t_value', 'log2FoldChange']
-    
-    cross_data = pd.merge(df1, df2, left_on='Partner', right_on='GeneName', how='inner')
-    
-    # Correlation matrix
-    cor_matrix = cross_data[col2draw].corr()
-    
-    # Plot heatmaps
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
-    
-    # Correlation heatmap
-    sns.heatmap(cor_matrix, ax=ax1, cmap='RdBu_r', vmin=-1, vmax=1, 
-                annot=True, square=True)
-    ax1.set_title('Correlation Matrix')
-    
-    # Data heatmap
-    sns.heatmap(cross_data[col2draw], ax=ax2, cmap='RdBu_r',
-                center=0, cbar_kws={'label': 't-score | lfc'})
-    ax2.set_title('Data Values')
-    
-    plt.tight_layout()
-    return cross_data
+    def cross_heatmap(self):
+        """Generate heatmap between two signature datasets"""        
+        # merge two datasets
+        cross_data = pd.merge(self.CDK6, self.RB1, on='Partner', how='inner',
+                              suffixes=('_CDK6', 'RB1'))
+        
+        # Correlation matrix
+        # cor_matrix = cross_data[col2draw].corr()
+        col2draw = cross_data.columns.str.match("t_value_*")
+        # data heatmaps
+        plt.figure(figsize=(10, 8))
+        sns.clustermap(cross_data.loc[:, col2draw], cmap='RdBu_r', row_cluster=True, col_cluster=False,
+                    center=0, cbar_kws={'label': 'CDK6 | RB1'})
+        plt.title('T score heatmap')
+        
+        plt.tight_layout()
+        return cross_data
 
 def set_presence(gene_set, care_data, seq_data, save_csv=False):
     """Analyze gene set presence in different datasets"""
